8343951c6923eb2d390a17f78757ae38f85d7a71
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-18 16:23:19 +0200

    add pad string code

--- csharp-oop-shop/Prodotti.cs
+++ csharp-oop-shop/Prodotti.cs
@@ -24,7 +24,7 @@ namespace csharp_oop_shop
 
 
         // STATI
-        //private bool onSale;
+        private bool onSale;
 
 
         // COSTRUTTORE
@@ -36,40 +36,58 @@ namespace csharp_oop_shop
             this.price = price;
             this.iva = iva;
 
-            //this.onSale = false;
+            this.onSale = true;
         }
 
 
 
-
+        //generare numero randomico
         private int GenerateCode()
         {
             Random rnd = new Random();
-            return rnd.Next(10000000, 99999999);
+            return rnd.Next(0, 99);
+            
+        }
+
+        //saldo prodotto 
+        public bool GetOnSale()
+        {
+            return this.onSale;
+        }
+        public bool SetOnSale()
+        {
+           return onSale = false;
         }
 
 
+        //get barcode
         public int Barcode
         {
-            get { return this.barcode; }
+         get { return this.barcode; }
         }
 
+
+        //get set name product
         public string Name
         {
             get { return this.name; }
             set { this.name = value; }
         }
 
+        //get set name description
         public string Description
         { get { return this.description; }
         set { this.description = value; } }
 
+
+        //get set price 
         public int Price
         {
             get { return this.price; }
             set { this.price = value; }
         }
 
+        //get set iva
         public int Iva
         {
             get { return this.iva; }
@@ -78,13 +96,47 @@ namespace csharp_oop_shop
 
         }
 
+        //calculate price + iva
         public int priceIva()
         {
             return this.price + ( this.price * iva / 100);
         }
 
+        //concat generatecode + name
+        public string extendName()
+        {
+            return  this.GenerateCode() + this.name;     
+        }
+
+        //pad string codeee
+        public string PadStringCode()
+        {       
+            
+                string codeString = barcode.ToString();
+           
+                int codelength = codeString.Length;
+
+            //  calcolo il numero di zeri da aggiungere come padding a sinistra del codice a barre sottraendo la lunghezza di "codeString" da 8
+            //  e memorizzando il risultato in una variabile
+
+                int add0 = 8 - codelength;
+
+            //Se "add0" è maggiore di 0
+
+            if (add0 > 0){
+
+                // la lunghezza del codice a barre è inferiore a
+                //8 caratteri e quindi  aggiunge dei padding a sinistra.
 
 
+                // quindi creo una nuova stringa formata da "add0" zeri seguiti dalla stringa "codeString",
+                //ottenendo così una nuova stringa con una lunghezza totale di 8 caratteri.
+
+                codeString = new string('0', add0) + codeString;
+
+            }
+              return codeString;
+        }
 
     }
 }
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,10 +1,14 @@
 ﻿
 using csharp_oop_shop;
 
-Prodotto prodotto1 = new Prodotto("shampoo", "Descrizione 1", 100, 22);
-Console.WriteLine("Codice: " + prodotto1.Barcode); // Codice generato casualmente
-Console.WriteLine(prodotto1.Name);
-Console.WriteLine(prodotto1.Description);
-Console.WriteLine(prodotto1.Price);
-Console.WriteLine(prodotto1.Iva + "%");
-Console.WriteLine(prodotto1.priceIva() + "%");
+Prodotto prodotto1 = new Prodotto("shampoo", "fantastico shampoo", 100, 22);
+Console.WriteLine("Codice a barre: " + prodotto1.Barcode); // Codice generato casualmente
+Console.WriteLine("Nome prodotto: "+ prodotto1.Name);
+Console.WriteLine("Descrizione prodotto: " + prodotto1.Description);
+Console.WriteLine("Prezzo base: "+ prodotto1.Price + "$");
+Console.WriteLine("Iva: "+ prodotto1.Iva + "%");
+Console.WriteLine("Prezzo finale con iva: "+ prodotto1.priceIva() + "$");
+Console.WriteLine("code piu' nome esteso: " + prodotto1.extendName());
+
+Console.WriteLine("numero pad0: " + prodotto1.PadStringCode());
+Console.WriteLine("il prodotto e' in saldo? " + prodotto1.SetOnSale());
